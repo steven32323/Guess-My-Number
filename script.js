1750d6cc41c564066383074a85ed33af47fc41ed
steven32323 <107620513+steven32323@users.noreply.github.com>
2022-11-02 09:27:56 +0000

    Added basic logic and error handling

--- script.js
+++ script.js
@@ -10,11 +10,20 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
   }
 });
